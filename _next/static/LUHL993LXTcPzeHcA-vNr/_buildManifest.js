87ae9f25a78ad81526bb75261ff4a38bc6eddc38
Tony Brobston <anthonybrobston@live.com>
2023-04-22 17:47:01 -0500

    Deploy

---
+++ _next/static/LUHL993LXTcPzeHcA-vNr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/394-cf2959664c08988e.js","static/chunks/pages/index-697e662fcedb8b9f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
